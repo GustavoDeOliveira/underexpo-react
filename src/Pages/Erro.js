b4f22dd0012f00115ba458a1e356f5638481596a
Gustavo de Oliveira <6ustavo.0liveira@gmail.com>
2025-11-25 22:16:59 -0300

    Corrigindo página de erro.

--- src/Pages/Erro.js
+++ src/Pages/Erro.js
@@ -1,7 +1,9 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Typography, CssBaseline, useMediaQuery } from '@mui/material';
 import { useRouteError } from "react-router-dom";
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
+import getTheme from '../theme';
 
 export const Erro = () => {
   const error = useRouteError();
@@ -14,23 +16,29 @@ export const Erro = () => {
       location.replace('/exposicoes');
       break;
   }
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = getTheme(prefersDarkMode);
+
   return (
-    
-    <Container>
-      <Box sx={{ backgroundColor: 'background.paper' }}>
-        <Header />
-        <Container maxWidth="md">
-          <Typography gutterBottom align="center" variant="h1">Epa!</Typography>
-          <Typography align="center">Ocorreu um erro inesperado.</Typography>
-          <Typography align="center">
-            <i>{error.statusText || error.message}</i>
-          </Typography>
-          <Typography align="center">
-            <i>{error.data}</i>
-          </Typography>
-        </Container>
-        <Footer />
-      </Box>
-    </Container>
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <Box sx={style => ({backgroundColor: style.palette.background.default, minHeight: '100vh'})}>
+          <Header />
+          <Container maxWidth="md">
+            <Typography gutterBottom align="center" variant="h1">Epa!</Typography>
+            <Typography align="center">Ocorreu um erro inesperado.</Typography>
+            <Typography align="center">
+              <i>{error.statusText || error.message}</i>
+            </Typography>
+            <Typography align="center">
+              <i>{error.data}</i>
+            </Typography>
+          </Container>
+          <Footer />
+        </Box>
+      </Container>
+    </CssVarsProvider>
   )
 }
